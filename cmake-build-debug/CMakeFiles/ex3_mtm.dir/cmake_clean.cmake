f04a930ccb0664867e9df6dfbc1799b552dfc3f6
Eliran T <dev.eliran96@gmail.com>
2020-01-06 11:47:59 +0200

    w

---
+++ cmake-build-debug/CMakeFiles/ex3_mtm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3_mtm.dir/tests/UniqueArrayTest.cpp.obj"
+  "ex3_mtm.exe"
+  "ex3_mtm.exe.manifest"
+  "ex3_mtm.pdb"
+  "libex3_mtm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex3_mtm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
